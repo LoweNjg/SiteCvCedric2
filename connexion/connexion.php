ba7e5129795e2f09a10c50fd5ab81b02d9f1dc79
LoweNjg <cedric.njonang@lepoles.com>
2017-07-07 16:56:09 +0200

    07/07/2017
    MISE A JOUR

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -1,14 +1,14 @@
 <?php
-	
+
 	$hote= 'localhost'; // le chemin vers le serveur
-	$bdd = 'siteCV_ktoure'; // le nom de la base de données
+	$bdd = 'sitecv_cnjonang'; // le nom de la base de données
 	$utilisateur= 'root'; // le nom d'utilisateur pour se connecter
 	$pass= ''; // mot de passe de l'utilisateur
-	
-	
+
+
 	$pdoCV= new PDO('mysql:host='.$hote.';dbname='.$bdd, $utilisateur, $pass);
 	//$pdoCV est le nom de la variable de la connexion qui sert partout o$ l'on doit se servir de cette connexion
 	$pdoCV->exec("SET NAMES utf8");
-	
+
 
 ?>
